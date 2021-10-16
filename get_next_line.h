e10313a363f697d2dc7f54f2dde49a0fd2b00c16
Alessandro Pangrazio <alessandropangrazio@live.com>
2021-10-16 18:53:57 +1030

    Ready for Testing

---
+++ get_next_line.h
@@ -0,0 +1,20 @@
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+#ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 5
+# endif
+
+# include <stdio.h>
+# include <unistd.h>
+# include <stdlib.h>
+
+size_t	ft_strlcpy(char *dst, const char *src, size_t dst)
+char	*ft_strchr(const char *s, int c)
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+char	*ft_strjoin(const char *s1, const char *s2)
+char	*ft_strdup(const char *s)
+size_t	ft_strlen(const char *s)
+char	*get_next_line(int fd)
+
+#endif
